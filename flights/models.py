eda6b5544199068937c0c1c067d5610447562443
agk007 <abhishekkodavalam@gmail.com>
2022-04-06 11:37:04 +0530

    git actions using yaml

--- flights/models.py
+++ flights/models.py
@@ -9,10 +9,19 @@ class Airport(models.Model):
         
 class Flight(models.Model):
     origin=models.ForeignKey(Airport, on_delete=models.CASCADE,related_name="departures")
-    destination=models.ForeignKey(Airport, on_delete=models.CASCADE,related_name="arrival")
+    destination=models.ForeignKey(Airport, on_delete=models.CASCADE,related_name="arrivals")
     duration=models.IntegerField()
 
     def __str__(self):#string representaion
         return f"{self.id}: {self.origin} to {self.destination}"  ##name to represent flight
+    
+    def is_valid_flight(self):
+        return self.origin != self.destination or self.duration >= 0
 
-    objects = models.Manager()
+class Passenger(models.Model):
+    first=models.CharField(max_length=64)
+    last=models.CharField(max_length=64)
+    flights=models.ManyToManyField(Flight,blank=True,related_name="passengers")    
+
+    def __str__(self):
+        return f"{self.first} {self.last}"
